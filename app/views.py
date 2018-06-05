507d3db4bbb0223d9f3cb7bc4552670bef0acc3c
RonaldZhao <zzgdata@gmail.com>
2018-06-05 23:29:11 +0800

    模板的if,for,继承

--- app/views.py
+++ app/views.py
@@ -1,7 +1,41 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, world!'
+    user = {'nickname': 'Ronald Zhao'}
+    comments = [
+        {
+            'user': 'user1',
+            'content': 'flask is very good.'
+        },
+        {
+            'user': 'user2',
+            'content': 'python is very good.'
+        },
+        {
+            'user': 'user2',
+            'content': 'python is very good.'
+        },
+        {
+            'user': 'user2',
+            'content': 'python is very good.'
+        },
+        {
+            'user': 'user2',
+            'content': 'python is very good.'
+        },
+        {
+            'user': 'user2',
+            'content': 'python is very good.'
+        }
+    ]
+    # return 'Hello, world!'
+    return render_template(
+        'index.html',
+        title='Home',
+        user=user,
+        comments=comments
+    )
