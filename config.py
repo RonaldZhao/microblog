ef79b05bfd8854ee9a044c43a20eb2950eb93403
RonaldZhao <zzgdata@gmail.com>
2018-06-07 12:41:35 +0800

    数据库

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
 CSRF_ENABLED = True
 SECRET_KEY = b'\xcdG\xa4\xbeI\xaf\x8b\x89?\t\xed\xfc\xfb\xca7\x0f'
 
